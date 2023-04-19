679dc6c4a1f4db7ed74b1ddc55f8115d2fc3afb4
jecndlria <jecndlria@gmail.com>
2023-04-19 02:14:21 -0700

    display works

--- project/project.ino
+++ project/project.ino
@@ -57,49 +57,62 @@ void initializeScreen()
   tft.initR(INITR_144GREENTAB);
   tft.fillScreen(ST7735_BLACK);
   tft.setTextColor(ST7735_WHITE);
+
+  tft.fillRect(0, 0, 128, 16, ST7735_BLACK);
+  tft.setCursor(0, 0);
+  tft.println("TEMPERATURE: ");
+
+  tft.fillRect(0, 16, 128, 32, ST7735_BLACK);
+  tft.setCursor(0, 16);
+  tft.println("HUMIDITY: ");
+
+  tft.fillRect(0, 32, 128, 48, ST7735_BLACK);
+  tft.setCursor(0, 32);
+  tft.println("WATER LEVEL: ");
+
+  tft.fillRect(0, 48, 128, 64, ST7735_BLACK);
+  tft.setCursor(0, 48);
+  tft.println("STATUS: ");
 }
+
 
 void lcdUpdateTemp()
 {
   tft.setTextColor(ST7735_WHITE);
-  tft.fillRect(0, 0, 128, 16, ST7735_BLACK);
-  tft.drawRect(69, 0, 128-69, 15, ST7735_WHITE); // change to fill black
-  tft.setCursor(0, 0);
-  tft.println("TEMPERATURE: ");
+  tft.fillRect(69, 0, 128-69, 15, ST7735_BLACK); // change to fill black
+  tft.setCursor(70, 0);
+  tft.print(random() % 100);
+
 }
 
 void lcdUpdateHumidity()
 {
   tft.setTextColor(ST7735_WHITE);
-  tft.fillRect(0, 16, 128, 32, ST7735_BLACK);
-  tft.drawRect(51, 16, 128-51, 15, ST7735_WHITE); // change to fill black
-  tft.setCursor(0, 16);
-  tft.println("HUMIDITY: ");
+  tft.fillRect(51, 16, 128-51, 15, ST7735_BLACK); // change to fill black
+  tft.setCursor(52, 16);
+  tft.print(random() % 100);
 }
 
 void lcdUpdateWaterLevel()
 {
   tft.setTextColor(ST7735_WHITE);
-  tft.fillRect(0, 32, 128, 48, ST7735_BLACK);
-  tft.drawRect(69, 32, 128-69, 15, ST7735_WHITE); // change to fill black
-  tft.setCursor(0, 32);
-  tft.println("WATER LEVEL: ");
+  tft.fillRect(69, 32, 128-69, 15, ST7735_BLACK); // change to fill black
+  tft.setCursor(70, 32);
+  tft.print(random() % 100);
 }
 
 void lcdUpdateHumidifier()
 {
   tft.setTextColor(ST7735_WHITE);
-  tft.fillRect(0, 48, 128, 64, ST7735_BLACK);
-  tft.drawRect(39, 48, 128-39, 15, ST7735_WHITE); // change to fill black
-  tft.setCursor(0, 48);
-  tft.println("STATUS: ");
+  tft.fillRect(39, 48, 128-39, 15, ST7735_BLACK); // change to fill black
+  tft.setCursor(40, 48);
+  tft.print(random() % 100);
 }
 /* 
 *
 * MOVE TO LCD.H
 *
 */
-int i = 0;
 
 
 void setup() {
@@ -107,15 +120,16 @@ void setup() {
   Serial.begin(9600);
   pinMode(BUTTON_PIN, INPUT);
   initializeScreen();
-  lcdUpdateTemp();
-  lcdUpdateHumidity();
-  lcdUpdateWaterLevel();
-  lcdUpdateHumidifier();
+
   //lcdUpdateReadings();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   if (digitalRead(BUTTON_PIN)) Serial.print("PRESSED\n");
-
+  lcdUpdateTemp();
+  lcdUpdateHumidity();
+  lcdUpdateWaterLevel();
+  lcdUpdateHumidifier();
+  delay(50);
 }
